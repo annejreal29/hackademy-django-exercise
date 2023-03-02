cbc6db83a462cfa33b293d565537dadfbb659a0a
annejreal29 <real.annejanette@gmail.com>
2023-03-02 09:56:43 +0800

    Week 2 Acitivty

--- activity1/views.py
+++ activity1/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def profile_view(request):
+    user = request.user
+    context = {
+        "user":user
+    }
+    return render(request,"profile.html", context)
